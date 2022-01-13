5bb0d8a95a8b09816542ef48372c866b5fef9072
Foram Patel <p.foram147@gmail.com>
2022-01-13 10:41:26 +0000

    initial

--- src/components/home/HomeNav.jsx
+++ src/components/home/HomeNav.jsx
@@ -5,42 +5,34 @@ import './home.css'
 const HomeNav = ()=>{
     return(
         <>
-       <Container fluid>
-       <Navbar collapseOnSelect className="navFont"  bg="light" variant="light" expand="sm">
-        
-        <Navbar.Brand href="#home" style={{marginLeft:'20px'}}>
-        <img
-          alt=""
-          src={logo}
-          width="70"
-          height="70"
-          className="d-inline-block align-top"
-          />{' '}
-        </Navbar.Brand>
-       <Navbar.Brand ><b>Resume Build</b></Navbar.Brand>
-      <Navbar.Toggle aria-controls='ressponsive-navbar-nav'/>
-      <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end" style={{marginRight:'140px'}} >
-        <Nav className="navNav" >
-          <Nav.Link  href="#features" >Resume</Nav.Link>
-          <Nav.Link  href="#pricing" >Cover Letter</Nav.Link>
-          <Nav.Link  href="#pricing" >Jobs</Nav.Link>
+       <div>
+       <Navbar collapseOnSelect className="navFont"  bg="light" variant="light" expand="md">
+          <Navbar.Brand href="#home" style={{marginLeft:'20px'}}>
+            <img alt="" src={logo} width="70" height="70" className="d-inline-block align-top"/>{' '}
+          </Navbar.Brand>
+          <Navbar.Brand ><b>Resume Build</b></Navbar.Brand>
+          <Navbar.Toggle aria-controls='ressponsive-navbar-nav'/>
+            <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end" style={{marginRight:'140px'}} >
+              <Nav className="navNav" >
+                <Nav.Link  href="#features" >Resume</Nav.Link>
+                <Nav.Link  href="#pricing" >Cover Letter</Nav.Link>
+                <Nav.Link  href="#pricing" >Jobs</Nav.Link>
       
-           <NavDropdown title="Career Advice" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-            <Button  variant="link">Sign In</Button>
-        <Button className="navBtn" variant="outline-success">Login</Button>
-     
+                <NavDropdown title="Career Advice" id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+                
+                
         </Nav>
         
       </Navbar.Collapse>
         
       </Navbar>
-      </Container>
+      </div>
             </>
  
     )
--- src/components/home/ToBegin.jsx
+++ src/components/home/ToBegin.jsx
@@ -5,18 +5,20 @@ const ToBegin =()=>{
     return(
         <>
 <div >
-<Card className="bg-dark text-white">
-  <Card.Img src={tobegin} style={{height:'35vw'}}  alt="Card image" />
-  <Card.ImgOverlay>
-    <h1 className="mt-1">Create a CV</h1>
-    
-    <Card.Text>
-      This is a wider card with supporting text below as a natural lead-in to
-      additional content. This content is a little bit longer.
-    </Card.Text>
-    <Card.Text>Last updated 3 mins ago</Card.Text>
-  </Card.ImgOverlay>
-</Card>
+  <Row>
+    <Col>
+      <Card className="bg-dark text-white">
+        <Card.Img src={tobegin} style={{height:'30vw'}}  alt="Card image" />
+          <Card.ImgOverlay>
+              <h1 className="mt-1">Create a CV</h1>
+             <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+            </Card.Text>
+            <Card.Text>Last updated 3 mins ago</Card.Text>
+          </Card.ImgOverlay>
+      </Card>
+    </Col>
+</Row>
 </div>
 </>
     )
